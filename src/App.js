0eca4440c2c6fa73170038f0df1d7e239bc74b80
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-12-27 16:58:32 +0530

    set state done

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ class App extends Component {
     {name:'ak',age:22},
     {name:'bk',age:23},
     {name:'ck',age:24},
-  ]
+  ],
+
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons:[
+        {name:'hks',age:19}
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
          <h1>hello reactjs</h1>
-         <Person name={this.state.persons[0].name} age={this.state.persons[1].age}></Person>
+         <button  onClick = {this.switchNameHandler}>CLICK me</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         </header>
         
       </div>
