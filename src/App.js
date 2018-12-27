f55a0fe35b8035600f52fff8d939cfaa4eac373e
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-12-27 16:36:41 +0530

    states done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+  persons:[
+    {name:'hk',age:21},
+    {name:'ak',age:22},
+    {name:'bk',age:23},
+    {name:'ck',age:24},
+  ]
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
          <h1>hello reactjs</h1>
-         <Person name="harshit" age = "21"/>
+         <Person name={this.state.persons[0].name} age={this.state.persons[1].age}></Person>
         </header>
         
       </div>
