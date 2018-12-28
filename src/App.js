e596d779f92f2a7c94b40b52833662abf5802f1f
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-12-28 18:52:28 +0530

    done with bind

--- src/App.js
+++ src/App.js
@@ -13,10 +13,17 @@ class App extends Component {
 
   }
 
-  switchNameHandler = () => {
+  changename = () =>{
+    return Math.ceil(Math.random()*4);
+  }
+
+  switchNameHandler = (newname) => {
     this.setState({
       persons:[
-        {name:'hks',age:19}
+        {name:newname,age:19},
+        {name:'bkss',age:221},
+        {name:'bks',age:232},
+        {name:'cks',age:242},
       ]
     })
   }
@@ -25,8 +32,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
          <h1>hello reactjs</h1>
-         <button  onClick = {this.switchNameHandler}>CLICK me</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+         <button  onClick = {this.switchNameHandler.bind(this,'dude')}>CLICK me</button>
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+          <Person name={this.state.persons[2].name} click = {this.switchNameHandler.bind(this,'hkss')}
+          age={this.state.persons[2].age}
+          ></Person>
+          <Person name={this.state.persons[3].name} age={this.state.persons[3].age}></Person>
         </header>
         
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,11 @@
 import React from 'react';
 const person = (props) => {
-   return <h1>Hello this is a {props.name} and {props.age} years old</h1>;
+   return(
+   <div>
+   <h1 onClick = {props.click}>Hello this is a {props.name} and {props.age} years old</h1>
+  
+</div>
+   );
 }
 
 export default person;
