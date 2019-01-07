e3c6600650047c4805750f218904a3dc3f1f51e2
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-01-07 22:56:51 +0530

    dynamic list added

--- src/App.js
+++ src/App.js
@@ -1,75 +1,80 @@
 import React, { Component } from 'react';
-import Person from './Person/Person';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-  persons:[
-    {name:'hk',age:21},
-    {name:'ak',age:22},
-    {name:'bk',age:23},
-    {name:'ck',age:24},
-  ],
-  showPerson:false
-
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  changename = () =>{
-    return Math.ceil(Math.random()*4);
-  }
-
-  switchNameHandler = (newname) => {
-    this.setState({
-      persons:[
-        {name:newname,age:19},
-        {name:'bkss',age:221},
-        {name:'bks',age:232},
-        {name:'cks',age:242},
+  switchNameHandler = ( newName ) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
       ]
-    })
+    } )
   }
-  
-  nameChangleHandler = (event) => {
-    this.setState({
-      persons:[
-        {name:'dsfd',age:19},
-        {name:event.target.value,age:221},
-        {name:'bks',age:232},
-        {name:'cks',age:242},
+
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
       ]
-    })
+    } )
   }
 
-  toggleHandler = () => {
-  
-      const doesshow = this.state.showPerson;
-      this.setState({showPerson:!doesshow})
-     }
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
 
-  render() {
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name} 
+              age={person.age}
+              />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-         <h1>hello reactjs</h1>
-         <button  onClick = {this.toggleHandler}>CLICK me</button>
-         {this.state.showPerson? 
-         <div>
-         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-          <Person name={this.state.persons[1].name} change = {this.nameChangleHandler}
-           age={this.state.persons[1].age}></Person>
-          <Person name={this.state.persons[2].name} click = {this.switchNameHandler.bind(this,'hkss')}
-          age={this.state.persons[2].age}
-          ></Person>
-          <Person name={this.state.persons[3].name} age={this.state.persons[3].age}></Person>
-          </div>
-          : <div><h1>NO Person</h1></div>}
-        </header>
-        
-        
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
-      
     );
-  
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
